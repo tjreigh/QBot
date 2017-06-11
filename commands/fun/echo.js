d2f2f5a84bdd8377fd2ea60b81cad2172c4c9a75
TJDoesCode <trevr03@gmail.com>
2017-06-11 16:41:45 -0500

    renamed key for argument to make more sense

--- commands/fun/echo.js
+++ commands/fun/echo.js
@@ -10,12 +10,12 @@ module.exports = class SuggestCommand extends commando.Command {
       memberName: 'echo',
       description: 'echos what you say!',
       details: oneLine `
-        Wanna make the bot say something? 
+        Wanna make the bot say something?
         Use this!
 			`,
       examples: ['echo lol'],
       args: [{
-          key: 'toSug',
+          key: 'toEcho',
           label: 'echo',
           prompt: 'What would you like me to say?',
           type: 'string',
@@ -32,7 +32,7 @@ module.exports = class SuggestCommand extends commando.Command {
           .setTitle(`:speaking_head: Echo:`)
           .setAuthor(`${message.author.tag}`, `${message.author.avatarURL}`)
           .setColor(0x0000FF)
-          .setDescription(`${args.toSug}`)
+          .setDescription(`${args.toEcho}`)
           .setFooter(`Message echoed from: ${message.author.username}`)
           .setTimestamp()
     message.channel.send({ embed });
