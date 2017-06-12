132cc6386755cb00b9903bc909956a77a54760b7
Chronomly6 <michaelgummere@gmail.com>
2017-06-11 20:26:23 -0500

    revert to base qtext

---
+++ commands/fun/qtext.js
@@ -0,0 +1,42 @@
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+const Discord = require('discord.js');
+function randomtext()
+{
+    let text = "";
+    let possible = "@#%@#@#%>?@#%>?%>@#%>??>?";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length * message.content.length));
+
+    return text;
+}
+
+module.exports = class SuggestCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'qtext',
+      group: 'fun',
+      memberName: 'qtext',
+      description: 'random qbert text!',
+      details: oneLine `
+        qbert like text!
+			`,
+      examples: ['qtext'],
+      args: [{
+        key: 'toQtext',
+        label: 'qtext',
+        prompt: 'Random Text',
+        type: 'string',
+        infinite: false
+      }],
+      guildOnly: true,
+      guarded: true
+    })
+  }
+
+  async run(message, args) {
+    let qbert = randomtext(message)
+    message.reply()
+  }
+};
