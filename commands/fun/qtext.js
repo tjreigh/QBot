83fe2ef64c56834ce3afa55839559f8529f1dc72
TJ The Developer <TJDoesCode@users.noreply.github.com>
2017-06-11 19:47:43 -0500

    Update

--- commands/fun/qtext.js
+++ commands/fun/qtext.js
@@ -37,6 +37,6 @@ module.exports = class SuggestCommand extends commando.Command {
 
   async run(message, args) {
     let qbert = randomtext()
-    message.reply(qbert)
+    message.reply(qbert * message.length)
   }
 };
